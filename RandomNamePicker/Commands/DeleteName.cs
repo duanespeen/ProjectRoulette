12b4b708f3f4281bccfec183933328f1a84c2b17
vosvosvos <indvfx@gmail.com>
2023-01-21 23:38:33 +0100

    Implement Delete

--- RandomNamePicker/Commands/DeleteName.cs
+++ RandomNamePicker/Commands/DeleteName.cs
@@ -1,17 +1,30 @@
 ﻿using RandomNamePicker.Abstractions;
+using RandomNamePicker.Receiver;
 
 namespace RandomNamePicker.Commands
 {
-    public class DeleteName : ICommand
+    public class DeleteName : IUndoCommand
     {
-        public DeleteName(string name)
+        private NameList _nameList;
+        private string _name;
+        public DeleteName(NameList nameList, string name)
         {
-
+            _nameList = nameList;
+            _name = name;
         }
-        
         public void Execute()
         {
-            throw new NotImplementedException();
+            _nameList.DeleteName(_name);
+        }
+
+        public void Undo()
+        {
+            _nameList.AddName(_name);
+        }
+
+        public void Redo()
+        {
+            Execute();
         }
     }
 }
--- RandomNamePicker/Program.cs
+++ RandomNamePicker/Program.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BackgroundServices;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using RandomNamePicker;
 using RandomNamePicker.Abstractions;
 using RandomNamePicker.Invoker;
 
