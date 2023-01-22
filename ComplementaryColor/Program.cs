603aa3fc46029308ba49c117b1af221e6e5a7c61
vosvosvos <indvfx@gmail.com>
2023-01-22 02:36:11 +0100

    ComplementaryColor

---
+++ ComplementaryColor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+/*Take Input
+use int.Parse and NumberStyles.HexNumber to convert the input to an int
+subtract from 0xFFFFFF to get the complement
+convert the int to a hex string with 6 digits
+*/
+
+string input = Console.ReadLine();
+int color = int.Parse(input, NumberStyles.HexNumber);
+color = 0xFFFFFF - color;
+Console.WriteLine(color.ToString("X6"));
