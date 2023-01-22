054fc1c9675c76cb2c65767c636f13d534338eda
vosvosvos <indvfx@gmail.com>
2023-01-22 03:44:48 +0100

    CaeserCipherCracker

---
+++ CaesarCipherCracker/Decoder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipherCracker
+{
+    public class Decoder
+    {
+        private string _input;
+        public Decoder(string input)
+        {
+            _input = input;
+        }
+        
+        public string Caeser(int shift)
+        {
+            char[] decoded = new char[_input.Length];
+            for(int i = 0; i < _input.Length; i++)
+            {
+                if (!char.IsLetter(_input[i])) decoded[i] = _input[i];
+                else
+                {
+                    char c = char.ToUpper(_input[i]);
+                    c = (char)(c - shift);
+                    if (c < 'A') c = (char)(c + 26);
+                    decoded[i] = char.IsUpper(_input[i]) ? c : char.ToLower(c);
+                }
+            }
+            return new string(decoded);
+        }
+    }
+}
---
+++ CaesarCipherCracker/Program.cs
@@ -0,0 +1,27 @@
+﻿using CaesarCipherCracker;
+
+Decoder decode = new("Kyzj zj r Trvjvi Tzgyvi Kvjk");
+Console.WriteLine("Do you know the shift? (y\\n) \n - If you select Y you will be asked to enter the shift \n - If you select N the program will attempt every possible shift");
+string input = Console.ReadLine().ToLower();
+switch (input)
+{
+    case "y":
+        Console.WriteLine("Enter the shift as an integer:");
+        int shift = int.Parse(Console.ReadLine());
+        if (shift > 26) shift = shift - 26;
+        Console.WriteLine(decode.Caeser(shift));
+        break;
+    case "n":
+        for (int i = 0; i < 26; i++)
+        {
+            Console.WriteLine($"Shift: {i} : {decode.Caeser(i)}");
+        }
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
+}
+
+
+
+
